93c9c74b4c36b6368583019319d9961ce25c75e5
weihl <weihule@163.com>
2022-11-17 23:13:44 +0800

    add some content

--- test.cpp
+++ test.cpp
@@ -32,37 +32,106 @@ int qiaozhuozi()
 {
     for (int i=1; i < 100; i++)
     {
-        if (i % 7 == 0)
+        if (i % 7 == 0 || i / 10 == 7 || i % 10 == 7)
         {
             cout << "敲桌子 " << i << endl;
         }
         else
         {
-            int number = 0;
-            int i_origin = i;
-            int i_number = i;
-            while (i_number > 0)
-            {
-                number = i_number % 10;    // 获得个位数
-                if (number == 7)
-                {
-                    cout << "敲桌子 " << i_origin << endl;
-                    break;
-                }
-                i_number = i_number / 10;
-            }
-            if (i_number == 0)
-            {
-                cout << i_origin << endl;
-            }
+            cout << i << endl;
         }
     }
     return 0;
 }
 
+int dayin()
+{
+    for (int i=0; i < 10; i++)
+    {
+        for (int j=0; j < 10; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 1;
+}
+
+int chengfakoujue()
+{
+    for (int i=1; i < 10; i++)
+    {
+        for (int j=1; j <= i; j++)
+        {
+            cout << j << "*" << i << "=" << i*j << "\t";
+        }
+        cout << endl;
+    }
+    return 1;
+}
+
+int xiaozhu()
+{
+    int arr[5] = {300, 350, 200, 400, 250};
+    int min_value = arr[0];
+    for (int i=1; i < 5; i++)
+    {
+        if (min_value > arr[i])
+        {
+            min_value = arr[i];
+        }
+    }
+
+    cout << "最小体重是：" << min_value << endl; 
+    return 1;
+}
+
+void nizhi()
+{
+    int arr[] = {1, 3, 2, 5, 4, 7};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    for (int i=0; i<length/2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[length-1-i];
+        arr[length-1-i] = temp;
+    }
+
+    for (int i=0; i<length; i++)
+    {
+        cout << arr[i];
+    }
+    cout << endl;
+}
+
+void bubble(void)
+{
+    int arr[] = {4, 2, 8, 0, 5, 7, 1, 3, 9};
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i=0; i < length-1; i++)
+    {
+        for (int j=0; j < length-1-i; j++)
+        {
+            if (arr[j] < arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    for (int i=0; i < length; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-    qiaozhuozi();
+    bubble();
     system("pause");
 
 }
